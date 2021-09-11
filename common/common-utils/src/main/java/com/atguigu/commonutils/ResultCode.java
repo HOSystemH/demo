dd3a413e69c7d19aef6dd899a97c1141252c28db
hosystem <hosystem@163.com>
2021-09-11 09:43:04 +0800

    统一结果返回

---
+++ common/common-utils/src/main/java/com/atguigu/commonutils/ResultCode.java
@@ -0,0 +1,8 @@
+package com.atguigu.commonutils;
+
+public class ResultCode {
+
+    public static Integer SUCCESS = 2000;
+
+    public static Integer ERROR = 20001;
+}
--- service/service-edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ service/service-edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -1,6 +1,7 @@
 package com.atguigu.eduservice.controller;
 
 
+import com.atguigu.commonutils.R;
 import com.atguigu.eduservice.entity.EduTeacher;
 import com.atguigu.eduservice.service.EduTeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,18 +28,21 @@ public class EduTeacherController {
     // 测试环境搭建是否成功
     //rest风格
     @GetMapping("/hello")
-    public List<EduTeacher> findAllTearcher(){
+    public R findAllTearcher(){
         //调用service 的方法实现查询所有的操作
         List<EduTeacher> list = eduTeacher.list(null);
-        return list;
+        return R.ok().data("items",list);
     }
 
     //删除逻辑讲师的方法
     @DeleteMapping("{id}")
-    public boolean removeTeacher(@PathVariable String id){
+    public R removeTeacher(@PathVariable String id){
         boolean flag = eduTeacher.removeById(id);
-        return flag;
+        if(flag){
+            return R.ok();
+        }else {
+            return R.error();
+        }
     }
-
 }
 
